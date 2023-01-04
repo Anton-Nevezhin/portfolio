25376224abb6723dd2a3bf798eb1488ae8bb38ac
anton-nevezhin <anton-nevezhin@yandex.ru>
2023-01-04 21:13:11 +0300

    Second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function menuopen() {
+    let btn = document.getElementById('menu-mob')
+    btn.setAttribute('style', 'display: block');
+    //btn.setAttribute.style.display = 'block'
+//alert('Клик!')
+}
+
+function menuclose() {
+    let btn = document.getElementById('menu-mob')
+    btn.setAttribute('style', 'display: none');
+    //btn.setAttribute.style.display = 'block'
+//alert('Клик!')
+}
